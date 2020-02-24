46fc81e4a3f7531b54bc87147838a44a0b62b430
Filipe Ferreira <filipregunta@hotmail.com>
2020-02-24 15:41:31 -0300

    Seção 22-Slim Framework

--- website/index.php
+++ website/index.php
@@ -1,5 +1,22 @@
 <?php
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+//use Slim\Slim;
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+
+    echo json_encode(array(
+        'date'=>date("d/m/Y")
+    ));
+
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
 ?>
