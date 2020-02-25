db9fa47a47ada94a2741b22b7abc1dd13ae086c1
Filipe Ferreira <filipregunta@hotmail.com>
2020-02-25 00:56:44 -0300

    Seção 23-Usando reCaptcha

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,8 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+
+    <div class="g-recaptcha" data-sitekey="6584LÇ6LcX79ALU8pakl8ACp_sr656565_l,=lljPPQq3"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+
+</form>
